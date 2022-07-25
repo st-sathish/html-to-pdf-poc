e59bb45fa7fac4397447cbb2e0d3706d1efa33ff
s-finalatix <sathish.t@finalatix.com>
2022-07-25 21:08:29 +0530

    form

---
+++ src/main/java/TestRepayment.java
@@ -0,0 +1,57 @@
+import com.itextpdf.html2pdf.HtmlConverter;
+import org.apache.commons.io.FileUtils;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestRepayment {
+    public static ClassLoaderTemplateResolver thymeleafTemplateResolver() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("LEGACYHTML5");
+        return templateResolver;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final TemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        Map<String, Object> data = new HashMap<>();
+        data.put("clientName", "client name heeel");
+        data.put("clientAddress", "client name heeel");
+        data.put("clientIdentificationNumber", "client name heeel");
+        data.put("clientDocumentIssueDate", "22-06-2022");
+        data.put("clientAuthority", "authoriey");
+        data.put("clientPin", "234567788");
+        data.put("companyName", "Infosyss");
+        data.put("attorneyNumber", "Infosyss");
+        data.put("attorneyDate", "Infosyss");
+        data.put("reasonPayment", "Infosyss");
+        data.put("loanId", "Infosyss");
+        data.put("loanCreationDate", "22-06-2022");
+        data.put("principalAmount", "60000");
+        data.put("loanAmountInWords", "Infosyss");
+        Context context = new Context();
+        context.setVariables(data);
+        String orderHtml = templateEngine.process("repayment", context);
+        System.out.println(orderHtml);
+        ByteArrayOutputStream target = new ByteArrayOutputStream();
+        /* Call convert method */
+        HtmlConverter.convertToPdf(orderHtml, target);
+
+        /* extract output as bytes */
+        byte[] bytes = target.toByteArray();
+        FileUtils.writeByteArrayToFile(new File("repayment.pdf"), bytes);
+//        final String filePath = "/home/daypay/workspace/intellij/mp-report-poc/reports/test.html";
+//                HtmlConverter.convertToPdf(
+//                new File(filePath),
+//                new File("test.pdf"));
+    }
+}

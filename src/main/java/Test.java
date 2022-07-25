e6d4807a75212da3226b47bb23a89fcf43ba8e12
sathish <sathish.2009btech@gmail.com>
2022-07-25 17:18:56 +0530

    fix

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -9,6 +9,8 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test {
 
@@ -23,8 +25,23 @@ public class Test {
     public static void main(String[] args) throws IOException {
         final TemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+        Map<String, Object> data = new HashMap<>();
+        data.put("clientName", "client name heeel");
+        data.put("clientAddress", "client name heeel");
+        data.put("clientIdentificationNumber", "client name heeel");
+        data.put("clientDocumentIssueDate", "22-06-2022");
+        data.put("clientAuthority", "authoriey");
+        data.put("clientPin", "234567788");
+        data.put("companyName", "Infosyss");
+        data.put("attorneyNumber", "Infosyss");
+        data.put("attorneyDate", "Infosyss");
+        data.put("reasonPayment", "Infosyss");
+        data.put("loanId", "Infosyss");
+        data.put("loanCreationDate", "22-06-2022");
+        data.put("principalAmount", "60000");
+        data.put("loanAmountInWords", "Infosyss");
         Context context = new Context();
-        context.setVariable("price", "23sath");
+        context.setVariables(data);
         String orderHtml = templateEngine.process("test", context);
         System.out.println(orderHtml);
         ByteArrayOutputStream target = new ByteArrayOutputStream();
